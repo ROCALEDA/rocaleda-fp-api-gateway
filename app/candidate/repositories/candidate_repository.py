d0fd1102350691f9f529d3718f753b39cec6ae1b
Camilo Sanchez <camilo.sanchez@heru-app.com>
2023-10-15 18:13:30 -0500

    Add try except on repository

--- app/candidate/repositories/candidate_repository.py
+++ app/candidate/repositories/candidate_repository.py
@@ -8,17 +8,21 @@ from app.commons.settings import settings
 class CandidateRepository:
     async def create_candidate(self, request: Request):
         async with httpx.AsyncClient() as client:
-            body = await request.json()
-            uri = self.__build_request_uri(settings.users_ms, "user/candidate/")
-            print(f"Sending {body} to {uri}")
-            response = await client.post(uri, json=body, timeout=60000)
+            try:
+                body = await request.json()
+                uri = self.__build_request_uri(settings.users_ms, "user/candidate/")
+                print(f"Sending {body} to {uri}")
+                response = await client.post(uri, json=body, timeout=60)
 
-            if 400 <= response.status_code < 600:
-                raise HTTPException(
-                    status_code=response.status_code, detail=response.text
-                )
+                if 400 <= response.status_code < 600:
+                    raise HTTPException(
+                        status_code=response.status_code, detail=response.text
+                    )
 
-            return response.json()
+                return response.json()
+            except Exception as e:
+                print(e)
+                raise HTTPException(status_code=500, detail="Internal Server Error")
 
     def __build_request_uri(self, host: str, endpoint: str) -> str:
         return f"https://{host}/{endpoint}"

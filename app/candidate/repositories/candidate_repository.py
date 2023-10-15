341a9caf02d4613ddb10c538f6f2101425272bc6
Camilo Alejandro Sánchez Cruz <ca.sanchezc12@uniandes.edu.co>
2023-10-15 17:28:45 -0500

    Add create candidate (#1) (#2)
    * Add create candidate
    
    * Remove dotenv explicit initialization
    
    * Update deployment
    
    ---------
    
    Co-authored-by: Camilo Sanchez <camilo.sanchez@heru-app.com>

---
+++ app/candidate/repositories/candidate_repository.py
@@ -0,0 +1,23 @@
+import httpx
+from fastapi import Request
+from fastapi import HTTPException
+
+from app.commons.settings import settings
+
+
+class CandidateRepository:
+    async def create_candidate(self, request: Request):
+        async with httpx.AsyncClient() as client:
+            body = await request.json()
+            uri = self.__build_request_uri(settings.users_ms, "user/candidate")
+            response = await client.post(uri, json=body)
+
+            if 400 <= response.status_code < 600:
+                raise HTTPException(
+                    status_code=response.status_code, detail=response.text
+                )
+
+            return response.json()
+
+    def __build_request_uri(self, host: str, endpoint: str) -> str:
+        return f"http://{host}/{endpoint}"

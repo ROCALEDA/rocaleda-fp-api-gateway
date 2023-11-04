f2d20fff8465491119f204115d740dafdc65c5e5
Camilo Sanchez <camilo.sanchez@heru-app.com>
2023-11-04 11:46:06 -0500

    Fix get candidates by filter path

--- app/candidate/repositories/candidate_repository.py
+++ app/candidate/repositories/candidate_repository.py
@@ -22,7 +22,7 @@ class CandidateRepository:
 
     async def get_candidates_paginated(self, request: Request):
         async with httpx.AsyncClient() as client:
-            uri = build_request_uri(settings.candidates_ms, "candidate")
+            uri = build_request_uri(settings.candidates_ms, "candidates")
             print(f"Sending {request.query_params} to {uri}")
             response = await client.get(uri, params=request.query_params, timeout=60)
 

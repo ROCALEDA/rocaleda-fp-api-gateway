0994b28442044a9e66ae0a6d8d9966f5137cef1a
Cristian Leonardo Toro Sarmiento <c.toros@uniandes.edu.co>
2023-11-03 22:01:28 -0500

    fix: ajustes llamado services

--- app/position/controllers/position_controller.py
+++ app/position/controllers/position_controller.py
@@ -21,4 +21,6 @@ def initialize(position_service: PositionService):
 
     @router.patch("/{position_id}")
     async def update_position_chosen_candidate(request: Request, position_id: int):
-        return await position_service.get_position_candidates_info(request, position_id)
+        return await position_service.update_position_chosen_candidate(
+            request, position_id
+        )

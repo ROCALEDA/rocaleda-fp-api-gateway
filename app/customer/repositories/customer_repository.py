5e8c43b17d0d9a87ddbd1c44c231a78db32a2e91
Camilo Alejandro Sánchez Cruz <ca.sanchezc12@uniandes.edu.co>
2023-11-18 14:54:32 -0500

    List technical tests (#46)
    * Add performance evaluations list (#39)
    
    * Add candidate to position (#41)
    
    * Create interview (#43)
    
    * List technical tests (#45)

--- app/customer/repositories/customer_repository.py
+++ app/customer/repositories/customer_repository.py
@@ -86,3 +86,36 @@ class CustomerRepository:
                     status_code=response.status_code, detail=error_detail
                 )
             return response.json()
+
+    async def get_customer_technical_tests(self, customer_id: int, request: Request):
+        async with httpx.AsyncClient() as client:
+            uri = build_request_uri(
+                settings.customers_ms,
+                f"customers/technical_tests/{customer_id}",
+            )
+            print(f"Sending request to {uri}")
+            response = await client.get(uri, timeout=60)
+
+            if 400 <= response.status_code < 600:
+                error_detail = response.json().get("detail", response.text)
+                raise HTTPException(
+                    status_code=response.status_code, detail=error_detail
+                )
+            return response.json()
+
+    async def get_candidate_technical_tests(self, candidate_id: int, request: Request):
+        async with httpx.AsyncClient() as client:
+            uri = build_request_uri(
+                settings.customers_ms,
+                f"customers/candidates/technical_tests/{candidate_id}",
+            )
+            print(f"Sending request to {uri}")
+            response = await client.get(uri, timeout=60)
+
+            if 400 <= response.status_code < 600:
+                error_detail = response.json().get("detail", response.text)
+                raise HTTPException(
+                    status_code=response.status_code, detail=error_detail
+                )
+            return response.json()
+
